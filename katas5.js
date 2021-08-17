20568c1039b510a5d1bad1965e010f2a6cf09a0c
glen1986 <escobar.glen@gmail.com>
2021-08-17 15:32:08 -0300

    katas5

--- katas5.js
+++ katas5.js
@@ -1,59 +1,190 @@
 // implemente aqui as funções de teste
+let str = "hola mono"
+let str1 = "Kenzie Academy" 
+let str2 = "pato amarillo"
+let arrayN = [2,7,4,1,5,6,3,8];
+let array2 = [4,6,3,7,9,1,5,2];
+let repeat = [2,2,4,6,1,3,3,7,8,5,5,5,3,2,4]
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const reverseString = (str) =>{
+   let newStr = str.split("").reverse().join("");
+//   console.log(newStr)
+   return newStr
+}
+reverseString(str);
+
 function testeReverseString1() {
    let result = reverseString("Kenzie Academy");
    let expected = "ymedacA eizneK";
    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
+testeReverseString1();
+
 function testeReverseString2() {
    let result = reverseString("Pato Amarillo");
    let expected = "olliramA otaP";
    console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+} testeReverseString2();
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const reverseSentence = () =>{
+  let newStr = str.split(" ").reverse().join(" ");
+//   console.log(newStr)
+   return newStr;
+} 
+ reverseSentence(str);
+
+function test_reverseSentence1() {
+      let result = reverseSentence("hola mono");
+      let expected = "mono hola";
+      console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
 }
+test_reverseSentence1();
+
+function test_reverseSentence2() {
+      let result = reverseSentence("hola mono");
+      let expected = "mono hola";
+      console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
+}
+test_reverseSentence2();
+ 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const minimumValue = () =>{
+   let narr = arrayN.sort((a, b) => a > b);
+   return narr[0]
+}
+minimumValue(arrayN)
+
 function testMinimumValue1(){
-   let result = array.sort((a,b) => a + b);
-   let expected= array[0];
-   console.assetr(result === expected; `esperado: ${expected}, obtido: ${result}`)
+    const tests = [
+        {
+            "input": arrayN,
+            "expected_result": 1
+        }
+    ]
+ tests.forEach((test) => console.assert(JSON.stringify(minimumValue(test.input)) === JSON.stringify(test.expected_result),
+                                            {
+                                                "Function": "minimumValue",
+                                                "expected": test.expected_result,
+                                                "got": minimumValue(test.input),
+                                                "tested": test.input
+                                            }))
 }
+testMinimumValue1();
 
 function testMinimumValue2(){
-   let result = array.sort((a,b) => b + a);
-   let expected = array[0];
-   console.assetr(result === expected; `esperado: ${expected}, obtido: ${result}`)
+    const tests = [
+        {
+            "input": array2,
+            "expected_result": 1
+        }
+    ]
+ tests.forEach((test) => console.assert(JSON.stringify(minimumValue(test.input)) === JSON.stringify(test.expected_result),
+        {
+            "Function": "minimumValue",
+            "expected": test.expected_result,
+            "got": minimumValue(test.input),
+            "tested": test.input
+        }))
 }
+testMinimumValue2();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const maximumValue = (arr) =>{
+   let narr = arr.sort((a, b) => a < b);
+   return narr[0]
+}
+maximumValue(array2)
+
+function testMaximumValue1(){
+   const tests = [
+      {
+         "input":arrayN,
+         "expected_result": 8
+      }
+   ]
+   tests.forEach((test) => console.assert(JSON.stringify(maximumValue(test.input)) === JSON.stringify(test.expected_result),
+      {
+         "function":"maximumValue",
+         "expected": test.expected_result,
+         "got" : maximumValue(test.input),
+         "tested" : test.input
+      }))
+}
+testMaximumValue1();
+
+function testMaximumValue2(){
+   const tests = [
+      {
+         "input":array2,
+         "expected_result": 9
+      }
+   ]
+   tests.forEach((test) => console.assert(JSON.stringify(maximumValue(test.input)) === JSON.stringify(test.expected_result),
+      {
+         "function":"maximumValue",
+         "expected": test.expected_result,
+         "got" : maximumValue(test.input),
+         "tested" : test.input
+      }))
+}
+testMaximumValue2();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const calculateRemainder = (arr) =>{
+   const resto = arr.filter(n => n % 2 === 0)
+   //console.log(resto)
+   return resto
+} 
+calculateRemainder(arrayN);
 
 function testCalculateRemainder1(){
-   let result = [1,2,3,4,5,6].filter(n => n % 2 === 0);
-   let expected = [2,4,6]; 
-   console.assetr(result === expected; `esperado: ${expected}, obtido: ${result}`)
+
+   const tests = [
+      {
+         "input":arrayN,
+         "expected_result":[8,6,4,2]
+      },
+      {
+         "input":array2,
+         "expected_result":[6,4,2]
+      }
+   ]
+   tests.forEach((test) => console.assert(JSON.stringify(calculateRemainder(test.input)) === JSON.stringify(test.expected_result),
+      {
+         "function":"calculateRemainder",
+         "expected": test.expected_result,
+         "got" : calculateRemainder(test.input),
+         "tested" : test.input
+      }))
 }
-function testCalculateRemainder2(){
-   let result = [1,2,3,4,5,6].filter(n => n % 2 !== 0);
-   let expected = [1,3,5]; 
-   console.assetr(result === expected; `esperado: ${expected}, obtido: ${result}`)
+testCalculateRemainder1();
+
+const distinctValues = () =>{
+  console.log(repeat)
+   let total = [];
+   for (let i = 0; i < repeat.length; i++){
+      total.push(repeat[i]);
+   }
+   console.log(total.reduce(i.index() === 1))
 }
-// 6 ///////////////
+distinctValues(repeat)
 function testDistinctValues1(){
-   let result = "1 3 5 3 7 3 1 1 5" ;
-   let expected = "1 3 5 7"; 
-   console.assetr(result === expected; `esperado: ${expected}, obtido: ${result}`)
-}
-function testDistinctValues2(){
-   let result = "1 3 5 3 7 3 1 1 5"
 
-}  
-// 7 ////////////////////
-function testCountValues1(){
-
-} 
-function testCountValues2(){
-
+   const tests = [
+      {
+         ////////
+      }
+      ]
+   tests.forEach((test) => console.assert(JSON.stringify(distinctValues(test.input)) === JSON.stringify(test.expected_result),
+      {
+         "function":"distinctValues",
+         "expected": test.expected_result,
+         "got" : distinctValues(test.input),
+         "tested" : test.input
+      }))
 }
-// 8 ////////////////////
-function testEvaluateExpression1() {
-   let result = evaluateExpression("a + b + c - d", {a: 1, b: 7, c: 3, d: 14});
-   let expected = -3
-   console.assert(result === expected, `esperado: ${expected}, obtido: ${result}`)
-}
-function testEvaluateExpression2(){
 
-}
+
+
+
